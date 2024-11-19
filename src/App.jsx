e857f149e7ca9261f183c7f62fa49bef4ed8abd1
Tharshen2124 <tharshensurianbalan@gmail.com>
2024-11-19 16:10:44 +0800

    chore: add and configure dotenv package

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,7 @@ import { useState } from 'react';
 import './App.css'
 
 function App() {
-  // provides 2 things -> variable, and a function that updates the variable
-  const [counter, setCounter] = useState(0);
-
-  const Adding = () => {
-    setCounter(prevCount => prevCount + 1);
-    console.log("counter", counter)
-  }
-
-  const Subtract = () => {
-    setCounter(prevCount => prevCount - 1);
-    console.log("counter", counter)
-  }
-
-  const Reset = () => {
-    setCounter(0);
-    console.log("counter", counter)
-  }
-
+  
   return (
     <>
       <h1>Number: {counter}</h1>
