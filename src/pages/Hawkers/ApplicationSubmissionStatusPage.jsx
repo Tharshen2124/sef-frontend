22fcd7eb37b0dd75011d8d6650f0ab8c8f3864ef
Tharshen2124 <tharshensurianbalan@gmail.com>
2025-01-21 01:48:46 +0800

    feat: create application submission status page

---
+++ src/pages/Hawkers/ApplicationSubmissionStatusPage.jsx
@@ -0,0 +1,57 @@
+import NavigationBar from '../../components/PublicUsers/NavigationBar'
+
+export default function ApplicationSubmissionStatusPage() {
+  const feedbackItems = [
+    {
+      businessName: "User spamming in forum posts",
+      ownerName: "Aiden Chan Kai Ming",
+      foodType: "cuisine",
+      status: "Under Review",
+    },
+    {
+      title: "False News",
+      ownerName: "Aiden Chan Kai Ming",
+      foodType: "cuisine",
+      status: "Rejected",
+    },
+  ]
+
+  return (
+    <>
+        <NavigationBar />
+        <div className="w-full p-10 mx-auto">
+        <h1 className="text-3xl font-bold mb-6">Application Submission Status</h1>
+        <table className="w-full">
+            <thead>
+            <tr className="text-left border-b border-[#999]">
+                <th className="py-2 w-[250px]">Hawker Business Name</th>
+                <th className="py-2 w-[600px] sm:pl-16 md:pl-24">Owner Name</th>
+                <th className="py-2 sm:pl-16 md:pl-24">Food Type</th>
+                <th className="py-2">Status</th> 
+                <th className="py-2 w-[100px]"></th>
+            </tr>
+            </thead>
+            <tbody>
+            {feedbackItems.map((item, index) => (
+                <tr key={index} className="border-b border-gray-200">
+                <td className="py-4">{item.title}</td>
+                <td className="py-6 sm:pl-16 md:pl-24">{item.ownerName}</td>
+                <td className="py-4 sm:pl-16 md:pl-24">{item.foodType}</td>
+                <td className="py-4">{item.status}</td>
+                <td className="py-4">
+                    <a 
+                    href="#" 
+                    className="text-blue-600 hover:text-blue-800 underline"
+                    >
+                    More Details
+                    </a>
+                </td>
+                </tr>
+            ))}
+            </tbody>
+        </table>
+        </div>
+    </>
+  )
+}
+
