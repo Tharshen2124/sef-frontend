5f1fd3bed4344b1445bb96c1d67f0a965a74ae0d
Suzannah <suzannahp123@gmail.com>
2025-01-30 18:19:38 +0800

    Done for hlmDashboardPage

---
+++ src/components/HLM/NavigationBarHLM.jsx
@@ -0,0 +1,22 @@
+import HawkTuahLogo from "../../assets/hawkTuahLogo.svg"
+
+export default function AdminNavigationBar() {
+    return (
+        <nav className="h-[70px] w-full border border-b[#e0e0e0] flex items-center justify-between px-10">
+            <img src={HawkTuahLogo} width="148px" alt="" />
+            <div className="flex items-center">
+                <li className="flex gap-x-8 transition ">
+                    <ul className="hover:text-blue-500"><a href="">Dashboard</a></ul>
+                    <ul className="hover:text-blue-500"><a href="">Hawker Applications</a></ul>
+                    <ul className="hover:text-blue-500"><a href="">Inspections Reports</a></ul>
+                    <ul className="hover:text-blue-500"><a href="">Manage Accounts</a></ul>
+                </li>
+            </div>
+            <div className="flex items-center gap-x-4">
+                <h2>HLM Mode</h2>
+                <b>|</b>
+                <button className="bg-yellow-300 py-2 px-8 rounded-md font-semibold">Logout</button>
+            </div>
+        </nav>
+    )
+}
--- src/pages/HLM/HLMDashboardPage.jsx
+++ src/pages/HLM/HLMDashboardPage.jsx
@@ -1,7 +1,39 @@
+import HLMNavigationBar from "../../components/HLM/NavigationBarHLM";
+import hawkerApplications from "../../assets/hawkerApplication.svg";
+import inspectionReports from "../../assets/inspectionReports.svg";
+import manageHawkerAccounts from "../../assets/manageHawkerAccounts.svg";
+import './HLMDashboardPage.css';
+
 import React from 'react'
 
-export default function HLMDashboardPage() {
+const HLMDashboardPage = () => {
   return (
-    <div>HLMDashboardPage</div>
-  )
-}
+    <><HLMNavigationBar/>
+    <div>
+    <div className="hlmDashboardPage">
+      <h1>Dashboard</h1>
+
+      <div className="hlmDPbuttons">
+          <button className="hawkerApplicationsbutton">
+              <img src={hawkerApplications} alt="test" />
+                <p>Hawker Application</p>
+          </button>            
+
+          <button className="inspectionsReportsbutton">
+            <img src={inspectionReports} alt="test" />
+              <p>Inspections Reports</p>
+          </button>
+
+          <button className="manageHawkerAccountsbutton">
+            <img src={manageHawkerAccounts} alt="test" />
+              <p>Manage Hawker Accounts</p>
+          </button>
+      </div>
+      </div>
+    </div>
+</>
+    
+  );
+};
+
+export default HLMDashboardPage;
